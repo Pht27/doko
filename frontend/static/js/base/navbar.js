c401658c01e81a0fd9c4da49ee1679b979c4679b
Pht <phili2709@gmail.com>
2025-05-30 21:31:50 +0200

    besseres schließverhalten navbar

--- frontend/static/js/base/navbar.js
+++ frontend/static/js/base/navbar.js
@@ -7,6 +7,14 @@ menuToggle.addEventListener("click", () => {
 });
 
 document.querySelectorAll(".overlay-menu a").forEach((link) =>
+  link.addEventListener("click", (e) => {
+    menuToggle.classList.remove("active");
+    overlayMenu.classList.remove("active");
+    e.stopPropagation();
+  })
+);
+
+document.querySelectorAll(".overlay-menu").forEach((link) =>
   link.addEventListener("click", () => {
     menuToggle.classList.remove("active");
     overlayMenu.classList.remove("active");
