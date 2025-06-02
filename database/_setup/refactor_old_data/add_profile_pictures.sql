05a22a4a8b3fa261c8a258ba3860f50abb62e8cf
Pht <phili2709@gmail.com>
2025-06-03 00:49:57 +0200

    leon pp added

--- database/_setup/refactor_old_data/add_profile_pictures.sql
+++ database/_setup/refactor_old_data/add_profile_pictures.sql
@@ -10,3 +10,5 @@ UPDATE player SET picture_name = "pB" WHERE id = 1;
 UPDATE player SET picture_name = "krK" WHERE id = 3;
 -- Aleks
 UPDATE player SET picture_name = "hK" WHERE id = 12;
+-- Leon
+UPDATE player SET picture_name = "hB" WHERE id = 8;
